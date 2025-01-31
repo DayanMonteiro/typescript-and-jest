a83f8ea304ee4c2411954bfda45a65382c78531a
DayanMonteiro <dsmmsmb@gmail.com>
2025-01-31 15:38:56 -0300

    type object

---
+++ TypescriptClasses/class01/src/A0004-type-object/index.ts
@@ -0,0 +1,18 @@
+const objectA: {
+  readonly keyA: string;
+  keyB: string;
+  keyC?: string;
+  [key: string]: unknown; // index signature
+} = {
+  keyA: 'Valor A',
+  keyB: 'Valor B',
+};
+
+objectA.keyB = 'Outro valor';
+objectA.keyC = 'Novo valor';
+objectA.keyD = 'Nova chave';
+
+console.log(objectA);
+
+// Module mode
+export default 1;
